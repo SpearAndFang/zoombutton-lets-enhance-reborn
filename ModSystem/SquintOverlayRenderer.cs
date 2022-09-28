e2cf5334627ae778d9a00a073e875a1ed463fd80
SpearAndFang <69551808+SpearAndFang@users.noreply.github.com>
2022-09-27 23:33:09 -0700

    Add files via upload

---
+++ ModSystem/SquintOverlayRenderer.cs
@@ -0,0 +1,118 @@
+using System;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+
+namespace ZoomButton
+{
+
+    /*
+    REFERENCE:
+    https://github.com/anegostudios/vsmodexamples/blob/master/Mods/ScreenOverlaySquintShader/src/ScreenOverlaySquintShader.cs
+    https://gist.github.com/patriciogonzalezvivo/670c22f3966e662d2f83
+    http://glslsandbox.com/e#71442.0
+    https://www.geeks3d.com/20091020/shader-library-lens-circle-post-processing-effect-glsl/
+    */
+
+    public class SquintOverlayRenderer : IRenderer
+    {
+        readonly MeshRef quadRef;
+        readonly ICoreClientAPI capi;
+        public IShaderProgram overlayShaderProg;
+
+        public float PercentZoomed = 0;
+
+        public SquintOverlayRenderer(ICoreClientAPI capi)
+        {
+            this.capi = capi;
+            var quadMesh = QuadMeshUtil.GetCustomQuadModelData(-1, -1, 0, 2, 2);
+            quadMesh.Rgba = null;
+            this.quadRef = capi.Render.UploadMesh(quadMesh);
+
+            this.LoadShader();
+            capi.Event.ReloadShader += this.LoadShader;
+            capi.Event.RegisterRenderer(this, EnumRenderStage.Ortho);
+        }
+
+        public double RenderOrder => 1.1;
+
+        public int RenderRange => 1;
+
+        public void Dispose()
+        {
+            this.capi.Render.DeleteMesh(this.quadRef);
+            this.overlayShaderProg.Dispose();
+        }
+
+        public void OnRenderFrame(float deltaTime, EnumRenderStage stage)
+        {
+            if (this.PercentZoomed == 0)
+                return;
+
+            var curShader = this.capi.Render.CurrentActiveShader;
+            curShader.Stop();
+            this.overlayShaderProg.Use();
+            this.capi.Render.GlToggleBlend(true);
+            this.overlayShaderProg.Uniform("percentZoomed", this.PercentZoomed);
+            this.capi.Render.RenderMesh(this.quadRef);
+            this.overlayShaderProg.Stop();
+            curShader.Use();
+        }
+
+        public bool LoadShader()
+        {
+            this.overlayShaderProg = this.capi.Shader.NewShaderProgram();
+            this.overlayShaderProg.VertexShader = this.capi.Shader.NewShader(EnumShaderType.VertexShader);
+            this.overlayShaderProg.FragmentShader = this.capi.Shader.NewShader(EnumShaderType.FragmentShader);
+
+            this.overlayShaderProg.VertexShader.Code = this.GetVertexShaderCode();
+            this.overlayShaderProg.FragmentShader.Code = this.GetFragmentShaderCode();
+
+            this.capi.Shader.RegisterMemoryShaderProgram("squintoverlay", this.overlayShaderProg);
+            this.overlayShaderProg.Compile();
+
+            return true;
+        }
+
+        private string GetVertexShaderCode()
+        {
+            return @"
+        #version 330 core
+        #extension GL_ARB_explicit_attrib_location: enable
+
+        #ifdef GL_ES
+        precision mediump float;
+        #endif
+
+        #extension GL_OES_standard_derivatives : enable
+
+        layout(location = 0) in vec3 vertex;
+
+        out vec2 uv;
+
+        void main(void) {
+          gl_Position = vec4(vertex.xy, 0, 1);
+          uv = (vertex.xy + 1.0) / 2.0;
+        }
+      ";
+        }
+
+        private string GetFragmentShaderCode()
+        {
+            return @"
+        #version 330 core
+
+        in vec2 uv;
+        out vec4 outColor;
+
+        uniform float percentZoomed;
+        uniform vec2 resolution;
+
+        void main () {
+          float dist = distance(uv.xy, vec2(0.5,0.5));
+          float viewStrength = smoothstep(0.45, 0.38, dist * smoothstep(-1, 1, percentZoomed));
+          outColor = vec4(0, 0, 0, min(0.8, 1 - viewStrength));
+        }
+      ";
+        }
+    }
+}
---
+++ ModSystem/ZoomButton.cs
@@ -0,0 +1,103 @@
+//using System;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+
+[assembly: ModInfo("ZoomButton")]
+
+namespace ZoomButton
+{
+    public class ZoomButton : ModSystem
+    {
+        private static readonly string HOTKEY_CODE = "zoombutton";
+        private static readonly string FIELD_OF_VIEW_SETTING_NAME = "fieldOfView";
+        private static readonly string MOUSE_SENSITIVITY_SETTING_NAME = "mouseSensivity"; // n.b. typo in Vintage Story's code!
+        private static readonly string MOUSE_SMOOTHING_SETTING_NAME = "mouseSmoothing";
+        private static readonly int MAX_FRAMERATE_MS = 1000 / 90;
+
+        private ICoreClientAPI capi;
+        private int originalFieldOfView;
+        private int originalMouseSensivity;
+        private int originalMouseSmoothing;
+        private bool isZooming = false;
+        private float zoomState = 0;
+        private ModConfig config;
+        private SquintOverlayRenderer renderer;
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            this.capi = api;
+
+            this.capi.Logger.Event("Hello from ZoomButton!");
+
+            // load config file or write it with defaults
+            this.config = api.LoadModConfig<ModConfig>("zoombutton.json");
+            if (this.config == null)
+            {
+                this.config = new ModConfig();
+                api.StoreModConfig(this.config, "zoombutton.json");
+            }
+
+            api.Input.RegisterHotKey(HOTKEY_CODE, "Zoom in", GlKeys.Z, HotkeyType.CharacterControls);
+            api.Event.RegisterGameTickListener(this.OnGameTick, MAX_FRAMERATE_MS);
+
+            this.renderer = new SquintOverlayRenderer(api);
+        }
+
+        private void OnGameTick(float dt)
+        {
+            var isHotKeyPressed = this.capi.Input.KeyboardKeyState[this.capi.Input.GetHotKeyByCode(HOTKEY_CODE).CurrentMapping.KeyCode];
+
+            // is the player currently zooming in?
+            if (isHotKeyPressed && this.zoomState < 1)
+            {
+                // is this the start of a zoom?
+                if (!this.isZooming)
+                {
+                    this.originalFieldOfView = this.capi.Settings.Int[FIELD_OF_VIEW_SETTING_NAME];
+                    this.originalMouseSensivity = this.capi.Settings.Int[MOUSE_SENSITIVITY_SETTING_NAME];
+                    this.originalMouseSmoothing = this.capi.Settings.Int[MOUSE_SMOOTHING_SETTING_NAME];
+                    this.isZooming = true;
+                }
+
+                // advance zoomState
+                this.zoomState += dt / this.config.zoomInTimeSec;
+                if (this.zoomState > 1)
+                {
+                    this.zoomState = 1; // clamp to 0..1
+                    isHotKeyPressed = false;
+                }
+                this.UpdateSettings();
+            }
+            // is the player currently zooming out?
+            else if (!isHotKeyPressed && this.zoomState > 0)
+            {
+                // advance zoomState
+                this.zoomState -= dt / this.config.zoomOutTimeSec;
+                if (this.zoomState < 0)
+                {
+                    this.zoomState = 0; // clamp to 0..1
+                    this.isZooming = false; // go back to initial state, which allows us to capture any player changes to settings
+                }
+                this.UpdateSettings();
+            }
+            this.renderer.PercentZoomed = this.zoomState;
+            // otherwise we are already zoomed all the way in or out: nothing to do
+        }
+
+        private void UpdateSettings()
+        {
+            // update fov and mouse sensitivity via linear interpolation based on zoomState 0..1
+            this.capi.Settings.Int[FIELD_OF_VIEW_SETTING_NAME] = this.lerp(this.originalFieldOfView, this.config.fieldOfView, this.zoomState);
+            this.capi.Settings.Int[MOUSE_SENSITIVITY_SETTING_NAME] = this.lerp(this.originalMouseSensivity, this.originalMouseSensivity * this.config.mouseSensitivityFactor, this.zoomState);
+            if (this.config.changeMouseSmoothing)
+            {
+                this.capi.Settings.Int[MOUSE_SMOOTHING_SETTING_NAME] = this.lerp(this.originalMouseSmoothing, this.config.mouseSmoothing, this.zoomState);
+            }
+        }
+
+        private int lerp(float a, float b, float t)
+        {
+            return (int)System.Math.Round(a + ((b - a) * t));
+        }
+    }
+}
